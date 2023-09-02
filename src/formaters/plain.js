fcf2fd0c85cc8167764b15d41bcd9eb453eac6b7
ismailibnaslanbek <isma.boss93@mail.ru>
2023-09-02 19:09:53 +0300

    plain formatter

--- src/formaters/plain.js
+++ src/formaters/plain.js
@@ -11,17 +11,21 @@ const stringify = (value) => {
 const plain = (ast) => {
   const iter = (tree, accKey) => {
     const list = tree.flatMap((node) => {
+      const {
+        value, valueOld, valueNew, children,
+      } = node;
+
       const path = [...accKey, node.key];
       const currentPath = path.join('.');
       switch (node.type) {
         case 'deleted':
           return `Property '${currentPath}' ${'was removed'}`;
         case 'added':
-          return `Property '${currentPath}' ${'was added with value'}: ${stringify(node.value)}`;
+          return `Property '${currentPath}' ${'was added with value'}: ${stringify(value)}`;
         case 'changed':
-          return `Property '${currentPath}' was updated. From ${stringify(node.valueOld)} to ${stringify(node.valueNew)}`;
+          return `Property '${currentPath}' was updated. From ${stringify(valueOld)} to ${stringify(valueNew)}`;
         case 'nested':
-          return `${iter(node.children, path)}`;
+          return `${iter(children, path)}`;
         case 'unchanged':
           return [];
         default:
